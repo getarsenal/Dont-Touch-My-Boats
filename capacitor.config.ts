07ea6c67a0963b16752ce56f7767300af8bc18ee
Connor Scheidel <getarsenal.app@gmail.com>
2026-05-17 16:47:58 -0400

    Add Capacitor iOS project + move web assets to www/

---
+++ capacitor.config.ts
@@ -0,0 +1,19 @@
+﻿import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scheidelholdings.dontouchmyboats',
+  appName: "Don't Touch My Boats",
+  webDir: 'www',
+  ios: {
+    contentInset: 'always',
+    scrollEnabled: false,
+    backgroundColor: '#080604',
+    allowsLinkPreview: false,
+  },
+  server: {
+    androidScheme: 'https',
+    iosScheme: 'capacitor',
+  },
+};
+
+export default config;
